575e6f9e8983f022ad2afe28d5fffc9a444c4220
rajrai <rajrai913197@gmail.com>
2026-05-13 20:38:26 +0530

    new updated

--- logic.js
+++ logic.js
@@ -1,15 +1,16 @@
 let submit=()=>{
     location.href = "main.html"
 }
-let btn=()=>{
-    location.href = "index.html"
-    return false
-   
-}
+let clickBtn=async()=>{
+    let inp = document.getElementById("inp").value.trim()
+    let apiKey = `7c6c0e0f18c17fdb3602fbdd8824ff99`
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${inp}&appid=${apiKey}&units=metric`
+    let res = await fetch(api);
+    let data = await res.json()
+    console.log(data);
+    location.href = "weather.html"
 
-let city = document.getElementById("loc-inp").value.trim()
-let key = "4b1f68f88e61fac1cbb6ba522b32c87a"
-let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+}
 
 
 
