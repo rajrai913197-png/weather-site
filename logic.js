45e0f10013f4ddf743149b13f338a2bd5f21e7c5
rajrai <rajrai913197@gmail.com>
2026-05-13 16:35:30 +0530

    new things

--- logic.js
+++ logic.js
@@ -7,5 +7,10 @@ let btn=()=>{
    
 }
 
+let city = document.getElementById("loc-inp").value.trim()
+let key = "4b1f68f88e61fac1cbb6ba522b32c87a"
+let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+
 
 
