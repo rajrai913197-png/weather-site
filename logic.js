0dc308bff9de0304ef6966ddd85ce522aa6affdb
rajrai <rajrai913197@gmail.com>
2026-05-15 22:20:32 +0530

    new conponent add

--- logic.js
+++ logic.js
@@ -3,21 +3,9 @@ let submit=()=>{
 }
 let clickBtn=async()=>{
     let inp = document.getElementById("inp").value.trim()
-    let apiKey = `7c6c0e0f18c17fdb3602fbdd8824ff99`
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${inp}&appid=${apiKey}&units=metric`
-    let res = await fetch(api);
-    let data = await res.json()
-    console.log(data.name);
      location.href = "weather.html"
-      let city = document.querySelector("#city")
-    city.innerHTML = "sagar"
-    
-    
-   
-   
+    localStorage.setItem("myCity",inp)
 
 }
- 
-
-
-
+let city = localStorage.getItem("myCity")
+console.log(city);
